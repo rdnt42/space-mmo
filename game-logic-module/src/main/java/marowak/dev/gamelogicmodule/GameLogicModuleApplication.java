73c91457cee02985c5d590f817a7345dbca2bcb5
rdnt42 <radiant42@yandex.ru>
2022-10-30 23:14:05 +0300

    1: added base config for game logic module

---
+++ game-logic-module/src/main/java/marowak/dev/gamelogicmodule/GameLogicModuleApplication.java
@@ -0,0 +1,13 @@
+package marowak.dev.gamelogicmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLogicModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameLogicModuleApplication.class, args);
+    }
+
+}
---
+++ game-logic-module/src/test/java/marowak/dev/gamelogicmodule/GameLogicModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package marowak.dev.gamelogicmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLogicModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
