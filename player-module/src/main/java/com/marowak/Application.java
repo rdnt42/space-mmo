24f79c2f8f5697e7d8e60e333572f1a07389ae8f
rdnt42 <radiant42@yandex.ru>
2022-11-16 22:57:22 +0300

    1: added base config for player module

---
+++ player-module/src/main/java/com/marowak/Application.java
@@ -0,0 +1,9 @@
+package com.marowak;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
