956c4ec49b8669e80a697d6d49f3007aa4b36d04
rdnt42 <radiant42@yandex.ru>
2022-11-20 22:49:15 +0300

    11: added flyway migration, script for table of users

---
+++ player-module/src/main/resources/db/migration/V1__users_create.sql
@@ -0,0 +1,12 @@
+create table if not exists users
+(
+    user_id           bigserial primary key,
+    username          text      not null unique,
+    password          text      not null,
+    is_activate       boolean   not null default false,
+    created_at        timestamp not null,
+    updated_at        timestamp not null,
+    last_logged_in_at timestamp not null
+);
+
+create index on users (username, password);
