581674cd8e8bac080393d9281c7d4902b0c85ff0
marowak <radiant42@yandex.ru>
2023-02-03 22:33:34 +0400

    added info for socket connection

--- client-module/src/websocket-service.js
+++ client-module/src/websocket-service.js
@@ -1,10 +1,27 @@
 import {MotionRequest, PlayerMotionRequest} from "./request/PlayerRequest.js";
 
-const webSocket = new WebSocket("ws://localhost:8082/motion/topic/pers2");
 
-webSocket.onclose = function () { console.log("WebSocket connection closed") };
+createConnectInfo();
 
-webSocket.onopen = function () { console.log("WebSocket connection opened") };
+function createConnectInfo() {
+    let input = document.createElement("input");
+    input.innerHTML = "InputText";
+    input.type = "text";
+    input.name = "inputText";
+    document.body.appendChild(input);
+
+    let btn = document.createElement("button");
+    btn.innerHTML = "Submit";
+    btn.type = "submit";
+    btn.name = "formBtn";
+    document.body.appendChild(btn);
+    btn.onclick = function () {
+        console.log("playerName: " + input.value)
+        initSocketConnection(input.value);
+        input.remove();
+        btn.remove();
+    };
+}
 
 export function sendMotion(x, y) {
     const motion = new MotionRequest(x, y);
@@ -13,11 +30,37 @@ export function sendMotion(x, y) {
     sendMessage(request);
 }
 
-//Send a message if it's not empty, then clear the input field
+function initSocketConnection(playerName) {
+    const webSocket = new WebSocket("ws://localhost:8082/motion/topic/" + playerName);
+    webSocket.onclose = onClose();
+    webSocket.onopen = onOpen();
+    webSocket.onmessage = onMessage();
+
+
+
+}
+
+function onClose() {
+    return function (p1) {
+        console.log("WebSocket connection closed");
+    };
+}
+function onOpen() {
+    return function (p1) {
+        console.log("WebSocket connection opened");
+    };
+}
+
+function onMessage() {
+    return function (event) {
+        console.log("get message", event);
+        const msg = JSON.parse(event.data);
+    };
+}
+
 function sendMessage(message) {
     if (message !== "") {
         webSocket.send(JSON.stringify(message));
         console.log("Send message: ", message);
     }
 }
-
