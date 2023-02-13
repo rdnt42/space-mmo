b9776679144fa9555c1d44bac3f0b0553139e3c5
marowak <radiant42@yandex.ru>
2023-02-14 00:10:34 +0300

    #19 removed unused functionality

--- client-module/src/obj/Player.js
+++ client-module/src/obj/Player.js
@@ -3,20 +3,10 @@ class Player {
         this.playerName = playerName;
         this.x = x;
         this.y = y;
-        this.prevX = x;
-        this.prevY = y;
         this.speed = 0;
         this.angle = 0;
         this.maxSpeed = 10;
     };
-
-    getDiffX() {
-        return this.x - this.prevX;
-    }
-
-    getDiffY() {
-        return this.y - this.prevY;
-    }
 
     getLocation() {
         return "X: " + this.x + " Y: " + this.y + " speed: " + this.speed + " angle: " + this.angle;
