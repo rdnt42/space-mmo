352eda490191a9ddd9cb7d5ad3ed508e36f327c4
marowak <radiant42@yandex.ru>
2023-02-17 21:26:40 +0300

    #33 added storage module

---
+++ storage-module/src/test/java/com/marowak/StorageModuleTest.java
@@ -0,0 +1,21 @@
+package com.marowak;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest(transactional = false)
+class StorageModuleTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
