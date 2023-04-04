3b32b2eae0b0ce349b82fb07ef5ed8beddc4f217
marowak <radiant42@yandex.ru>
2023-04-04 22:51:20 +0400

    #53 fixed engine field

--- storage-module/src/main/resources/db/migration/V5__engines_create.sql
+++ storage-module/src/main/resources/db/migration/V5__engines_create.sql
@@ -2,7 +2,7 @@ create table if not exists engines
 (
     engine_id      bigserial,
     is_active      bool     not null,
-    character_id   text references characters (character_name),
+    character_name text references characters (character_name),
     engine_type_id smallint references engine_types (engine_type_id),
     speed          smallint not null,
     upgrade_level  smallint not null,
